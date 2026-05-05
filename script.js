c262c30836b917764aee39f78665172bacf69287
Clara Sousa <clara3.macedo@gmail.com>
2026-05-04 23:17:07 -0300

    ajustes cores e botões

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+        duration: 800,  
+        once: true,     
+        offset: 100,    
+    });
+});
+
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
